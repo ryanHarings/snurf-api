1558c451e417c0fb781d949a086bf1177c9da09a
Ryan Harings <ryanharings@hotmail.com>
2016-12-07 12:00:04 -0700

    heroku deployment

--- src/server/routes/accounts.js
+++ src/server/routes/accounts.js
@@ -6,20 +6,6 @@ const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
 
 // ********** dev only **********
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' === req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-router.use(allowCrossDomain);
 
 router.post('/register', function (req, res, next) {
   console.log('reg.body', req.body);
